8b6bbbceef18cc43531cc0371e37b04131d1f3be
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-30 23:48:01 +0000

    Task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,8 @@
 """ """
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
-    """ """
-    pattern = f"({'|'.join([re.escape(field) + r'=.*?(?={separator}|$)' for field in fields])})"
-    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
+    pattern = f"({'|'.join(f'{re.escape(field)}=.*?(?={re.escape(separator)}|$)' for field in fields)})"
+    return re.sub(
+            pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
