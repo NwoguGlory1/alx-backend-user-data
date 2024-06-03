b62a44841622116b0a762d6f4eca61e395558ba0
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-03 09:39:32 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,10 +5,15 @@ import logging
 
 
 def filter_datum(fields, redaction, message, separator) -> str:
-    pattern = f"({'|'.join(f'{re.escape(field)}=.*?(?={re.escape(separator)}|$)' for field in fields)})"
+    pattern = (
+        f"({'|'.join(f'{re.escape(field)}=.*?(?={re.escape(separator)}|$)' for field in fields)})"
+    )
     return re.sub(
-            pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
-    
+        pattern,
+        lambda m: m.group().split('=')[0] + '=' + redaction,
+        message
+    )
+
     class RedactingFormatter(logging.Formatter):
         """ Redacting Formatter class """
 
