57b32b5a49ff27f2b61fdd3b4b7e29acbcc438e1
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-08 15:49:03 +0100

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,9 +16,9 @@ class SessionAuth(Auth):
         if not isinstance(user_id, str):
             return None
 
-    session_id_generated = uuid.uuid4()
-    """ Generates a new UUID using uuid4()"""
-    session_id = str(session_id_generated)
-    """ Convert the UUID to a string before using as key"""
-    self.user_id_by_session_id[session_id] = user_id
-    return session_id
+        session_id_generated = uuid.uuid4()
+        """ Generates a new UUID using uuid4()"""
+        session_id = str(session_id_generated)
+        """ Convert the UUID to a string before using as key"""
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
