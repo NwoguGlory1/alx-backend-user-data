69e73ffe82ff65c689aeadce62adba26e9914e0f
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-08 16:13:55 +0100

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,3 +22,13 @@ class SessionAuth(Auth):
         """ Convert the UUID to a string before using as key"""
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ method that returns a User ID based on a Session ID"""
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        """ .get() is used to access the value, user_id for the key session_id """
+        user_id = self.user_id_by_session_id.get(session_id)
+        
