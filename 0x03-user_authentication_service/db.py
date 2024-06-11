91d8735a5f91fe3cd9052096e9757b3815c60f0b
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-11 22:35:18 +0100

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from typing import TypeVar
 
-from user import Base
-from user import User
+from user import Base, User
 
 class DB:
     """DB class
