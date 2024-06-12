39ae877ef92435861619eb1e78e0e5d750da8d9b
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-12 04:54:27 +0100

    Find user

--- 0x03-user_authentication_service/paste.py
+++
@@ -1,7 +0,0 @@
-    def find_user_by(**kwargs):
-        """
-         method takes in arbitrary keyword 
-         returns first row in users table,
-         filtered by the method’s input args
-        """
-        
