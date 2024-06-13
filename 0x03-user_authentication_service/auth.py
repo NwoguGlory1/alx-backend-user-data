5ce27f9cc0f3e706b09532001684067bcbc414d9
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-13 15:33:16 +0100

    . Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,8 +4,11 @@
 import bcrypt
 
 def _hash_password(password: str) -> bytes:
-    """ """
+    """
+    method to use bcrypt.hashpw to
+    generate a salted hash of the input password.
+    """
     password_in_bytes = password.encode("UTF-8")
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-    bcrypt.hashpw(password, salt)
+    hashed_password = bcrypt.hashpw(password_in_bytes, salt)
+    return hashed_password
