53a7ccec95c869d0f3480da23a5874635dbc4547
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-14 11:12:19 +0000

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app script
 """
-from flask import Flask,abort, jsonify, request
+from flask import Flask, abort, jsonify, request
 from auth import Auth
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -31,27 +31,29 @@ def users():
     except ValueError:
         return jsonify({"message": "User already exists"}), 400
 
+
 @app.route('/sessions', methods=['POST'])
 def login():
     """Handles user login"""
     # Get the request data fields
     email = request.form['email']
     password = request.form['password']
-    
+
     # Validate the login credentials
     if AUTH.valid_login(email, password):
         # If credentials are valid, create a new session
         session_id = AUTH.create_session(email)
-        
+
         # Set the session ID as a cookie on the response
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie("session_id", session_id)
-        
+
         # Return the JSON payload
         return response
     else:
         # If credentials are invalid, return 401 error
         abort(401)
+
 
 if __name__ == "__main__":
     """ ensures script is run directly """
